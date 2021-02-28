291243adc52d8104d74a40df9f30c89d4971ff7e
TheMaynMike <79784730+TheMaynMike@users.noreply.github.com>
2021-02-28 19:12:35 +0500

    Source Code

--- main.cpp
+++ main.cpp
@@ -1,11 +1,8 @@
 #include <windows.h>
-#include <time.h>
-#include <iostream>
 
 int main()
-{ 
-	HWND hwnd = GetDesktopWindow();
-	HDC hdc = GetDC(hwnd);
+{
+	HDC hdc = GetDC(GetDesktopWindow()); 
 	int ScrW = GetSystemMetrics(SM_CXSCREEN);
 	int ScrH = GetSystemMetrics(SM_CYSCREEN);
 
@@ -13,14 +10,17 @@ int main()
 	{
 		for (int i = 0;; i++, i %= 3)
 		{
-			int y = rand() % ScrH;
-			HBRUSH brush = CreateSolidBrush(RGB(rand() % 255, rand() % 255, rand() % 255));
-			SelectObject(hdc, brush);
-			PatBlt(hdc, 0, y, ScrW, 300, PATINVERT);
-			BitBlt(hdc, 0, y, ScrW, 300, hdc, rand() % 31 - 10, y, SRCCOPY);
-			if (!i)RedrawWindow(0, 0, 0, 133);
+			for (int c = 0; c < rand() % 16; c++)
+			{
+				int y = rand() % ScrH;
+				HBRUSH brush = CreateSolidBrush(RGB(rand() % 255, rand() % 255, rand() % 255));
+
+				SelectObject(hdc, brush);
+				BitBlt(hdc, 0, y, ScrW, 300, hdc, rand() % 61 - 30, y, SRCCOPY);
+				PatBlt(hdc, 0, y, ScrW, 300, PATINVERT);
+			}
+
+			if (!i) RedrawWindow(0, 0, 0, 133);
 		}
 	}
-
-	return 0;
 }
